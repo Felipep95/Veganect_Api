e987f94c032266bbb485a2c80d6d182e71c30bee
Felipe Pereira <felipe.pereiralf95@gmail.com>
2020-11-07 17:39:52 -0300

    feature/[000.002.000] created a model classes and initial configure of database

---
+++ VeganectApi/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeganectApi.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Display(Name = "Nome")]
+        [Required( ErrorMessage ="O campo nome é obrigatório")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public List<Recipe> Recipes = new List<Recipe>();
+    }
+}
---
+++ VeganectApi/Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeganectApi.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Recipe> Recipies { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseNpgsql(ConnectionString: @"Server=127.0.0.1 ;Port=5432; Database=VeganectApi; User Id=postgres; Password=postgres;");
+        //}
+
+    }
+}
